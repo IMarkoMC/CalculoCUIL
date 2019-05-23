91f5b0125b15c8868e12bf6c76c0a5c3658b9aa5
Marco <marco@druetta.com>
2019-05-23 08:27:12 -0300

    First commit

---
+++ app/src/main/java/me/marco/calculocuil/MainActivity.java
@@ -0,0 +1,13 @@
+package me.marco.calculocuil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
